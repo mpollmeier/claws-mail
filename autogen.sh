0caf2e38276c4c9a7c19bea88faddfb0fbc98d88
colinler <colinler>
2007-01-25 12:48:51 +0000

    warn about deprecated code

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-
+echo
+echo Warning:
+echo
+echo Development takes place on the \'gtk2\' cvs branch.
+echo You problably don\'t want to compile the old gtk1 branch.
+echo Use the \'-r gtk2\' parameter in the \'cvs checkout\' command.
+echo
 autopoint \
   && aclocal -I m4 \
   && libtoolize --force --copy \
